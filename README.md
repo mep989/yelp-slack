4bdeaf8e17be8697243d9457462104f9e9f9a58b
Chris Birk <cmbirk@gmail.com>
2015-12-21 17:11:31 -0500

    add initial commands for bot

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # yelp-slack
 Yelp Slackbot
+
+## Installation
+1.  Copy `example.creds.json` to `creds.json`
+1.  Add your bot API token to creds.json
+1.  Run `node index.js`
--- index.js
+++ index.js
@@ -4,7 +4,16 @@
 
 var botkit = require('botkit');
 var yelp = require('yelp');
+var creds = require('./creds.json');
 
-console.log("hello");
+var controller = botkit.slackbot({
+  debug:true
+});
 
+controller.spawn({
+  token: creds.slackbot_token
+}).startRTM;
 
+controller.hears('hungry', 'direct_message,direct_mention,mention', function (bot, message) {
+  bot.reply(message, "What are you hungry for?");
+});
