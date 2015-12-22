a9a3447c89cc467d9e6eadf931ae04e02fc02f82
Jimmy Steinmetz <jimmy.steinmetz@gmail.com>
2015-12-22 18:02:11 -0500

    Added conversation for asking what kind of food user wants

--- bot.js
+++ bot.js
@@ -17,6 +17,23 @@ controller.spawn({
   token: process.env.SLACKBOT_TOKEN
 }).startRTM();
 
-controller.hears('hungry', 'direct_message,direct_mention,mention', function (bot, message) {
-  bot.reply(message, "What are you hungry for?");
-});
+//controller.hears('hungry', 'direct_message,direct_mention,mention', function (bot, message) {
+//  bot.reply(message, "What are you hungry for?");
+//});
+
+controller.hears(['hungry'],['direct_message','direct_mention','mention','ambient'],function(bot,message) {
+
+	  // start a conversation to handle this response.
+	  bot.startConversation(message,function(err,convo) {
+
+	    convo.ask('What are you hungry for?',function(response,convo) {
+
+	      convo.say('Mmmmm, ' + response.text);
+        convo.say("Let's see what's looks good around you.")
+	      convo.next();
+
+	    });
+
+	  })
+
+	});
